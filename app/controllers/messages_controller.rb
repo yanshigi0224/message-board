57cd07c294dc259e63089851e7b319c09655d7c9
Shinya Katagi <yanshigi.2.24@gmail.com>
2017-01-21 18:41:06 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,14 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path , notice:"メッセージを保存しました．"
+  else
+    @messages=Message.all
+    flash.now[:alert] = "メッセージの保存に失敗しました．"
+    render "index"
   end
+end
 
   private
   def message_params
